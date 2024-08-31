fa5a9c335f402a87c00e69dde42a42ca5e589e03
Igor Khotin <khotin@gmx.com>
2024-09-01 02:10:18 +0300

    [+] orbital controller pod

---
+++ mod/jet/com/OrbitalControllerPod.js
@@ -0,0 +1,130 @@
+class OrbitalControllerPod {
+
+    constructor(st) {
+        extend(this, {
+            name:      'mover',
+            speed:     20,
+            turnSpeed: 2,
+            r:         10,
+        }, st)
+
+        this.pushers = new Float32Array(SHIFT_ROLL+1)
+    }
+
+    capture() {
+        lab.broker = this
+    }
+
+    init() {
+        this.capture()
+    }
+
+    push(action, factor, dt) {
+        const __ = this.__
+        const speed     = this.speed
+        const turnSpeed = this.turnSpeed
+
+        switch(action) {
+            case FORWARD:
+                __.moveZ(-speed * dt)
+                break
+            case STRAFE_LEFT:
+                __.moveX(-speed * dt)
+                break
+            case BACKWARD:
+                __.moveZ(speed * dt)
+                break
+            case STRAFE_RIGHT:
+                __.moveX(speed * dt)
+                break
+            case FLY_UP:
+                __.moveY(speed * dt)
+                break
+            case FLY_DOWN:
+                __.moveY(-speed * dt)
+                break
+
+            /*
+            case LOOK_LEFT:
+                __.yaw(-turnSpeed * dt)
+                break
+            case LOOK_RIGHT:
+                __.yaw(turnSpeed * dt)
+                break
+            case LOOK_UP:
+                __.pitch(-turnSpeed * dt)
+                break
+            case LOOK_DOWN:
+                __.pitch(turnSpeed * dt)
+                break
+            case ROLL_LEFT:
+                __.roll(turnSpeed * dt)
+                break
+            case ROLL_RIGHT:
+                __.roll(-turnSpeed * dt)
+                break
+
+            case SHIFT_YAW:
+                __.yaw(-turnSpeed * factor * dt)
+                break
+            case SHIFT_PITCH:
+                __.pitch(turnSpeed * factor * dt)
+                break
+            case SHIFT_ROLL:
+                __.roll(turnSpeed * factor * dt)
+                break
+            */
+        }
+    }
+
+    evo(dt) {
+        // activate pushers
+        for (let i = 0; i < this.pushers.length; i++) {
+            const f = this.pushers[i]
+            if (f) {
+                this.push(i, f, dt)
+                if (i > 20) this.pushers[i] = 0 // reset the mouse movement accumulation buffers
+            }
+        }
+    }
+
+    activate(action) {
+        this.pushers[action] = 1
+    }
+
+    stop(action) {
+        this.pushers[action] = 0
+    }
+
+    onMouseDown(e) {
+        /*
+        if (e.button == 0) {
+            if (!env.mouseLock) captureMouse()
+        }
+        */
+    }
+
+    onMouseUp(e) {}
+
+    onMouseMove(e) {
+        /*
+        if (!env.mouseLock) return
+
+        const dx = e.movementX, dy = e.movementY
+
+        if (dx) {
+            if (e.shiftKey) {
+                // accumulate mouse roll
+                this.pushers[SHIFT_ROLL] += dx * .1
+            } else {
+                // accumulate horizontal mouse movement
+                this.pushers[SHIFT_YAW] -= dx * .1
+            }
+        }
+        if (dy) {
+            // accumulate vertical mouse movement
+            this.pushers[SHIFT_PITCH] += dy * 0.075
+        }
+        */
+    }
+}
--- mod/sandbox/boxCorkscrew.js
+++ mod/sandbox/boxCorkscrew.js
@@ -1,9 +1,32 @@
 _screwing = true
 
+function createSomeBoxes() {
+    for (let i = 0; i < 70; i++) {
+        const B = 200
+        const H = B/2
+        lab.attach( new Body({
+            pos: vec3(
+                H - B*rnd(),
+                H - B*rnd(),
+                H - B*rnd()
+            ),
+            rot: vec3(0, 0, 0),
+            scale: vec3(1, 1, 1),
+
+            _pods: [
+                new Mesh({
+                    geo: geo.gen().cube().scale(4 + rnd() * 4).bake(),
+                }),
+            ],
+        }))
+    }
+}
+
+
 _.boxCorkscrew = function() {
     log('Setting up editing mode...')
 
-    lab.attach( new Body({
+    const box = lab.attach( new Body({
         pos: vec3(
             0,
             0,
@@ -27,6 +50,8 @@ _.boxCorkscrew = function() {
 
     }))
 
+    createSomeBoxes()
+
     // move camera back a little
     lab.cam.pos[1] = 0
     lab.cam.pos[2] = 7
@@ -36,7 +61,9 @@ _.boxCorkscrew = function() {
     }
     kill(lab.cam.mover)
 
-    env.directionalLightVector = vec3(1, 1, 1)
+    lab.cam.attach( new OrbitalControllerPod() )
+    lab.cam.lookAt = box.pos
 
+    env.directionalLightVector = vec3(1, 1, 1)
     env.pointLightPosition = vec3(5, -4, -5)
 }
