5583315728651c43a18243b32fd1d6209386699c
Igor Khotin <khotin@gmx.com>
2024-09-07 10:28:56 +0300

    [*] move geometry from geo dsl to screw

--- mod/stage/glib.js
+++ mod/stage/glib.js
@@ -1,14 +1,16 @@
 function zapGeoLib() {
+    /*
     geo.gen()
         .cube()
         .pushv([.5, .5, .1])
         .stretchZ().stretchY().stretchX()
         .push('floppy').name()
         .bake()
+    */
+    screw('gen cube 0.5 0.5 0.1 stretchZ stretchY stretchX "floppy" name brew')
     glib.floppy.bounds = vec3(1.5, 1.5, 1)
 
-    const s =  CELL_HSIZE, // block half-size
-          h =  1
+    /*
     geo.gen().cube()
         .pushv([s, h, s])
         .stretchX()
@@ -16,6 +18,10 @@ function zapGeoLib() {
         .stretchZ()
         .push('cell').name()
         .bake()
+    */
+    const s =  CELL_HSIZE, // block half-size
+          h =  1
+    screw('gen cube 8 1 8 stretchX stretchY stretchZ "cell" name brew')
     glib.cell.bounds = vec3(s, h, s)
 
     if (debug) {
