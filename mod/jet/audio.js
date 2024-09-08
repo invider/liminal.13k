13d099e713ccb6f9bc26ce8d6a0872ee49777979
Igor Khotin <khotin@gmx.com>
2024-09-08 13:27:28 +0300

    [+] modular synth engine

--- mod/jet/audio.js
+++ mod/jet/audio.js
@@ -4,13 +4,55 @@ const fx = (() => {
 let aout, dt, R,
     currentChannel = 0
 const
-    ch  = [],
     smp = [],  // samples
     // SFX source functions
     f = [
         // noise
         () => 2*rnd() - 1,
-    ]
+    ],
+
+    // patch route structure
+    TARGET    = 0,    // target oscillator, -1 if a carrier oscillator
+    TYPE      = 1,    // wave type
+    FQM       = 2,    // frequency modifier
+    ATTACK    = 3,    // attack time
+    PEAK      = 4,    // attack peak value
+    DECAY     = 5,    // decay time
+    SUSTAIN   = 6,    // sustain value
+    RELEASE   = 7,    // release time
+
+    SIN      = 0,
+    SQR      = 1,
+    SAW      = 2,
+    TRI      = 3,
+    NSE      = 4,
+    OSC      = [
+        'sine',
+        'square',
+        'sawtooth',
+        'triangle'
+    ],
+    p = [
+        // 0 - sine wave
+        [
+            // route target, wave, attack time, peak, decay time/value, release time
+            // TAR-TYPE-FQM-ATK-PIK-DCY-SUS-REL
+            [   -1, SIN,  1, .2,  1, .3, .5, 1 ],
+        ],
+        [
+            // TAR-TYPE-FQM-ATK-PIK-DCY-SUS-REL
+            [   -1, SAW,  1, .2,  1, .3, .5, .2 ],
+        ],
+        [
+            // TAR-TYPE-FQM-ATK-PIK-DCY-SUS-REL
+            [   -1, TRI,  1, .2,  1, .3, .5, .2 ],
+        ],
+        [
+            // TAR-TYPE-FQM-ATK-PIK-DCY-SUS-REL
+            [   -1, SQR,  1, .2,  1, .3, .5, .2 ],
+        ],
+    ],
+    ch = []
 
 // === SFX ===
 function createSample(s, f) {
@@ -23,6 +65,115 @@ function createSample(s, f) {
     smp.push(b)
 }
 
+function touch() {
+    if (!aux || aux.state === 'suspended') {
+        aux = new AudioContext()
+        aout = aux.destination
+        R    = aux.sampleRate
+        dt   = 1/R
+
+        // generate samples - one for each sample function
+        f.map(x => createSample(x))
+    }
+}
+
+function play(note, fq, at, len, ipatch) {
+    // create the patch routing table
+    const routes = [], gains = []
+    if (!fq) fq = 440 * (2 ** ((note - 45)/12))
+    log('#' + fq)
+
+    const masterGain = aux.createGain()
+    masterGain.gain.value = env.vol
+    masterGain.connect(aout)
+
+    p[ipatch].forEach((patch, i) => {
+        const osc = aux.createOscillator()
+        osc.type = OSC[ patch[TYPE] ]
+        osc.frequency.setValueAtTime(fq * patch[FQM], at)
+
+        const envelope = aux.createGain()
+        envelope.gain.setValueAtTime(0, at)
+        envelope.gain.linearRampToValueAtTime(patch[PEAK], at + patch[ATTACK])
+        envelope.gain.linearRampToValueAtTime(patch[SUSTAIN], at + patch[ATTACK] + patch[DECAY])
+        osc.connect(envelope)
+
+        // route oscilator -> gain -> ...
+        patch[TARGET] < 0? envelope.connect(masterGain) : gain.connect(routes[ patch[TARGET] ])
+
+        // start oscilator at scheduled time
+        osc.start(at)
+
+        if (len) {
+            // scheduling
+            envelope.gain.setValueAtTime(patch[SUSTAIN], at + len)
+            envelope.gain.linearRampToValueAtTime(0, at + len + patch[RELEASE])
+            osc.stop(at + len + patch[RELEASE]) // TODO wait for the note-off if len is not specified
+        }
+
+        routes[i] = osc
+        gains[i] = envelope
+    })
+
+    // live play
+    if (!len) for (let i = 1; i < 64; i++) {
+        if (!ch[i]) {
+            ch[i] = {
+                patch: p[ipatch],
+                routes,
+                gains,
+            }
+            return i
+        }
+    }
+
+    //const carrier = aux.createOscillator()
+    //carrier.type = "sine"
+    //carrier.frequency.value = fq
+
+    //const modulator = aux.createOscillator()
+    //modulator.type = "triangle"
+    //modulator.frequency.value = fq * .25
+
+    // setup the FM
+    //const mgain = aux.createGain()
+    //mgain.gain.value = 3000
+
+    // route the modulation for the signal
+    //modulator.connect(mgain)
+    //mgain.connect(carrier.frequency) // FM
+
+    //const volumeGain = aux.createGain()
+    //volumeGain.gain.value = env.vol
+    //volumeGain.connect(aout)
+
+    //carrier.connect(volumeGain)
+
+    //modulator.start(at)
+    //carrier.start(at)
+
+    //modulator.stop(at + len)
+    //carrier.stop(at + len)
+}
+
+function playin(note, fq, delay, len, patch) {
+    return play(note, fq, aux.currentTime + delay, len, patch)
+}
+
+function off(id) {
+    const n = ch[id], at = aux.currentTime
+    n.patch.forEach((patch, i) => {
+        n.gains[i].gain.setValueAtTime(patch[SUSTAIN], at)
+        n.gains[i].gain.linearRampToValueAtTime(0, at + patch[RELEASE])
+        n.routes[i].stop(at + patch[RELEASE])
+        n.kt = at + patch[RELEASE]
+    })
+    setTimeout(() => {
+        ch[id] = null
+    }, 5000)
+}
+
+/*
 function createChannel(n) {
     const channel = ch[n] = {}
 
@@ -54,72 +205,7 @@ function createChannel(n) {
     channel.modulator.start()
     channel.carrier.start()
 }
-
-function touch() {
-    if (!aux) {
-        aux = new AudioContext()
-        aout = aux.destination
-        R    = aux.sampleRate
-        dt   = 1/R
-        createChannel(0)
-
-        // generate samples - one for each sample function
-        f.map(x => createSample(x))
-    }
-    /*
-    const modOsc1Gain = ctx.createGain()
-    modOsc1Gain.gain.value = 3000
-    modOsc1.connect(modOsc1Gain)
-
-    modOsc1Gain.connect(carOsc.frequency)
-    */
-}
-
-function on(note) {
-    const channel = ch[currentChannel]
-    channel.carrier.frequency.value = 440 + (440/12) * note
-    channel.modulator.frequency.value = channel.carrier.frequency.value * .25
-    channel.volumeGain.gain.value = env.vol 
-}
-
-function off() {
-    const channel = ch[currentChannel]
-    channel.volumeGain.gain.value = 0
-}
-
-function play(fq, at, len, patch) {
-    const carrier = aux.createOscillator()
-    carrier.type = "sine"
-    carrier.frequency.value = fq
-
-    const modulator = aux.createOscillator()
-    modulator.type = "triangle"
-    modulator.frequency.value = fq * .25
-
-    // setup the FM
-    const mgain = aux.createGain()
-    mgain.gain.value = 3000
-
-    // route the modulation for the signal
-    modulator.connect(mgain)
-    mgain.connect(carrier.frequency) // FM
-
-    const volumeGain = aux.createGain()
-    volumeGain.gain.value = env.vol
-    volumeGain.connect(aout)
-
-    carrier.connect(volumeGain)
-
-    modulator.start(at)
-    carrier.start(at)
-
-    modulator.stop(at + len)
-    carrier.stop(at + len)
-}
-
-function playin(note, delay, len, patch) {
-    play(note, aux.currentTime + delay, len, patch)
-}
+*/
 
 const sfx = (n) => {
     const bufs = aux.createBufferSource(),
@@ -134,10 +220,9 @@ const sfx = (n) => {
 
 extend(sfx, {
     touch,
-    on,
-    off,
     play,
     playin,
+    off,
 })
 return sfx
 
--- mod/sandbox/boxAudio.js
+++ mod/sandbox/boxAudio.js
@@ -1,4 +1,74 @@
+const
+      // 1 octave
+      C1  = 0,
+      C$1 = 1,
+      D1  = 2,
+      D$1 = 3,
+      E1  = 4,
+      F1  = 5,
+      F$1 = 6,
+      G1  = 7,
+      G$1 = 8,
+      A1  = 9,
+      A$1 = 10,
+      B1  = 11,
+      // 2 octave
+      C2  = 12,
+      C$2 = 13,
+      D2  = 14,
+      D$2 = 15,
+      E2  = 16,
+      F2  = 17,
+      F$2 = 18,
+      G2  = 19,
+      G$2 = 20,
+      A2  = 21,
+      A$2 = 22,
+      B2  = 23,
+      // 3 octave
+      C3  = 24,
+      C$3 = 25,
+      D3  = 26,
+      D$3 = 27,
+      E3  = 28,
+      F3  = 29,
+      F$3 = 30,
+      G3  = 31,
+      G$3 = 32,
+      A3  = 33,
+      A$3 = 34,
+      B3  = 35,
+      // 4 octave
+      C4  = 36,
+      C$4 = 37,
+      D4  = 38,
+      D$4 = 39,
+      E4  = 40,
+      F4  = 41,
+      F$4 = 42,
+      G4  = 43,
+      G$4 = 44,
+      A4  = 45,
+      A$4 = 46,
+      B4  = 47,
+      // 5 octave
+      C5  = 48,
+      C$5 = 49,
+      D5  = 50,
+      D$5 = 51,
+      E5  = 52,
+      F5  = 53,
+      F$5 = 54,
+      G5  = 55,
+      G$5 = 56,
+      A5  = 57,
+      A$5 = 58,
+      B5  = 59
+
+let _patch = 1
+
 _.boxAudio = () => {
+
 
     function discoBall() {
         log('add a disco ball :)')
@@ -45,47 +115,59 @@ _.boxAudio = () => {
         lab.broker = null  // disable cam controls
     }
 
+    let octave = 2, transpose = 0
+
     function playSequence() {
-        const notes = 'ABD.ABDD....BDEF...TGDDG...WWW...ETYT....aabie'
-        const fqs = notes.split('').map(c => {
+        const pattern = 'ABD.ABDD....BDEF...TGDDG...WWW...ETYT....aabie'
+        const notes = pattern.split('').map(c => {
             if (c === '.') return 0
-            return 440*1.06**(c.charCodeAt(0) - 65)
+            return (c.charCodeAt(0) - 65 + 12)
         })
 
         const step = .25
         const start = aux.currentTime
-        fqs.forEach(((f, i) => {
-            if (f) fx.play(f, start + i*step, step, 0)
+        notes.forEach(((n, i) => {
+            if (n) fx.play(n, 0, start + i*step, step, 1)
         }))
     }
 
+    function on(N) {
+        off(N)
+        n = N + (octave * 12) + transpose
+        const channel = fx.play(n, 0, aux.currentTime, 0, _patch)
+        keyActive[N] = channel
+    }
+
+    function off(N) {
+        const channel = keyActive[N]
+        if (channel) {
+            fx.off(channel)
+            keyActive[N] = 0
+        }
+    }
+
+    const keyActive = []
+
+    const keyMap = [
+        'KeyZ', 'KeyS', 'KeyX', 'KeyD', 'KeyC',
+        'KeyV', 'KeyG', 'KeyB', 'KeyH', 'KeyN', 'KeyJ', 'KeyM',
+        'KeyQ', 'Digit2', 'KeyW', 'Digit3', 'KeyE',
+        'KeyR', 'Digit5', 'KeyT', 'Digit6', 'KeyY', 'Digit7', 'KeyU',
+    ]
+
     function synthControls() {
         trap.register('keyDown', (e) => {
-            switch(e.code) {
-                case 'KeyQ': fx.on(0); break;
-                case 'KeyW': fx.on(1); break;
-                case 'KeyE': fx.on(2); break;
-                case 'KeyR': fx.on(3); break;
-                case 'KeyT': fx.on(4); break;
-                case 'KeyY': fx.on(5); break;
-                case 'KeyU': fx.on(6); break;
-                case 'KeyI': fx.on(7); break;
-                case 'KeyO': fx.on(8); break;
-                case 'KeyP': fx.on(9); break;
-                case 'KeyA': fx.on(10); break;
-                case 'KeyB': fx.on(11); break;
+            const i = keyMap.indexOf(e.code)
+            if (i >= 0) on(i)
 
-                case 'KeyZ': playSequence(); break;
+            switch(e.code) {
+                case 'Home': playSequence(); break;
             }
         })
 
         trap.register('keyUp', (e) => {
-            switch(e.code) {
-                case 'KeyQ': case 'KeyW': case 'KeyE': case 'KeyR':
-                case 'KeyT': case 'KeyY': case 'KeyU': case 'KeyI':
-                case 'KeyO': case 'KeyP': case 'KeyA': case 'KeyB':
-                    fx.off(); break;
-            }
+            const i = keyMap.indexOf(e.code)
+            if (i >= 0) off(i)
         })
     }
 
